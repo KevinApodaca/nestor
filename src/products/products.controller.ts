538b339434469585ba2530f893bc835e3ae70af7
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-03-29 15:11:03 -0600

    :pencil2: fixed typo

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -27,7 +27,7 @@ export class ProductsController {
   }
 
   @Get(':id')
-  getProduct(@Param('id') prodId: string,){
+  getProduct(@Param('id') prodId: string){
     return this.productsService.getSingleProduct(prodId);
   }
 }
